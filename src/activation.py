360c0ac9dc4ed9d7f90ff5fe65ba004169451688
d-gurgurov <dgurgurov@gmail.com>
2025-07-03 12:59:53 +0200

    added support to more models in activation

--- src/activation.py
+++ src/activation.py
@@ -7,6 +7,7 @@ from vllm import LLM, SamplingParams
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model", type=str, default="meta-llama/Meta-Llama-3-8B")
 parser.add_argument("-l", "--lang", type=str, default="zh")
+parser.add_argument("-s", "--save", type=str, default="llama_3-1")
 args = parser.parse_args()
 
 is_llama = bool(args.model.lower().find('llama') >= 0)
@@ -14,7 +15,7 @@ model = LLM(model=args.model, tensor_parallel_size=torch.cuda.device_count(), en
 
 max_length = model.llm_engine.model_config.max_model_len
 num_layers = model.llm_engine.model_config.hf_config.num_hidden_layers
-intermediate_size = model.llm_engine.model_config.hf_config.intermediate_size if is_llama else model.llm_engine.model_config.hf_config.hidden_size * 4
+intermediate_size = model.llm_engine.model_config.hf_config.intermediate_size
 
 over_zero = torch.zeros(num_layers, intermediate_size, dtype=torch.int32).to('cuda')
 
@@ -45,32 +46,16 @@ def factory(idx):
         x, _ = self.down_proj(x)
         return x
 
-
-    def bloom_forward(self, x: torch.Tensor):
-        x, _ = self.dense_h_to_4h(x)
-        x = self.gelu_impl(x)
-        activation = x.float()
-        over_zero[idx, :] += (activation > 0).sum(dim=(0,1))
-        x, _ = self.dense_4h_to_h(x)
-        return x
-
-    if is_llama:
-        return llama_forward
-    else:
-        return bloom_forward
+    return llama_forward
 
 for i in range(num_layers):
-    if is_llama:
-        obj = model.llm_engine.model_executor.driver_worker.model_runner.model.model.layers[i].mlp
-    else:
-        obj = model.llm_engine.engine_core.model.model.transformer.h[i].mlp
+    obj = model.llm_engine.model_executor.driver_worker.model_runner.model.model.layers[i].mlp
     obj.forward = MethodType(factory(i), obj)
 
 lang = args.lang
-if is_llama:
-    ids = torch.load(f'data_llama_3-1/id.{lang}.train.llama-3.1')
-else:
-    ids = torch.load(f'data/id.{lang}.train.bloom')
+save = args.save.split(" ")
+ids = torch.load(f'data_{save[0]}/id.{lang}.train.{save[1]}')
+
 l = ids.size(0)
 l = min(l, 99999744) // max_length * max_length
 input_ids = ids[:l].reshape(-1, max_length)
@@ -79,5 +64,4 @@ output = model.generate(prompt_token_ids=input_ids.tolist(), sampling_params=Sam
 
 output = dict(n=l, over_zero=over_zero.to('cpu'))
 
-if is_llama:
-    torch.save(output, f'data_llama_3-1/activation.{lang}.train.llama-3.1')
+torch.save(output, f'data_{save[0]}/activation.{lang}.train.{save[1]}')
